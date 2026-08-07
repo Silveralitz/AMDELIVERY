33587c5dd60e2745883c8c582d45a9b6d93ae3b6
Silveralitz <www.matias0902@gmail.com>
2026-08-07 15:29:52 -0300

    intento arreglo telefono

--- checkout.js
+++ checkout.js
@@ -62,9 +62,12 @@ checkoutForm.addEventListener('submit', async (event) => {
         total
     };
 
-    const apiUrl = window.location.origin === 'http://localhost:3000'
-        ? `${window.location.origin}/api/order`
-        : 'http://localhost:3000/api/order';
+    const isLocalFile = window.location.protocol === 'file:';
+    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    const hostForApi = isLocalFile || isLocalhost
+        ? 'localhost'
+        : window.location.hostname;
+    const apiUrl = `http://${hostForApi}:3000/api/order`;
     console.log('Enviando pedido a', apiUrl);
 
     try {
